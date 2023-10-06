8cfea65584fe5453e034725b0fa2f659fc63742e
Ayot0 <samuelondimu79@gmail.com>
2023-10-06 09:22:16 -0700

    numbers again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point 
+ *
+ * Return: program execution
+ */
+int main(void)
+{
+int number = 0;
+while (number <= 9)
+{
+putchar(number + '0');
+number++;
+}
+putchar('\n');
+return 0;
+}
