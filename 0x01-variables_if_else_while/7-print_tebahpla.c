3f3592c45ec7f9723610b14725aba89cb38d43fe
Ayot0 <samuelondimu79@gmail.com>
2023-10-06 09:39:28 -0700

    smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: program execution
+ */
+int main(void)
+{
+char letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
+}
+putchar('\n');
+return 0;
+}
