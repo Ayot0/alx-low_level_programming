261b1f5e7fffbc5c5d5ea40859acfc0752dbdf01
Ayot0 <samuelondimu79@gmail.com>
2023-10-06 08:45:03 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: program execution
+ */
+int main(void)
+{
+int number = 0;
+while (number <= 9)
+{
+printf("%d\n", number);
+number++;
+}
+return (0);
+}
