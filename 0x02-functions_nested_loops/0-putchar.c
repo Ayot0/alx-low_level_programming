3618f1cb582556859386c0b8660f9abccada771b
Ayot0 <samuelondimu79@gmail.com>
2023-10-12 08:50:36 -0700

    grrr

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - entry point
  *
  * Return: program execution
  */
+
 int main(void)
 {
-char text[] = "_putchar\n";
-write(1, text, sizeof(text) - 1);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
 
