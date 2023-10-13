206be3b23aee99723cf06349141553ae92a3083c
Ayot0 <samuelondimu79@gmail.com>
2023-10-13 02:33:19 -0700

    hack three

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function to multiply two integers
+ *
+ * @a: parameter 1
+ *
+ * @b: parameter 2
+ *
+ * Return: a * b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
